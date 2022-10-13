ac5434cb6f2b0fda84ad3c0f28927f48df34cc15
Alesia <andomyel@yandex.ru>
2022-10-13 22:00:09 +0300

    HW4C#task2

--- HW4C#/Program.cs
+++ HW4C#/Program.cs
@@ -1,6 +1,6 @@
 ﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 
-Console.WriteLine("Введите положительное число А");
+/*Console.WriteLine("Введите положительное число А");
 int A = int.Parse(Console.ReadLine()!);
 
 Console.WriteLine("Введите положительное число В");
@@ -22,4 +22,31 @@ int Power(int num1, int num2){
     }
     return result;
 
+}*/
+
+//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе
+
+Console.WriteLine("Введите положительное число");
+string num = Console.ReadLine()!;
+
+if (num[0] == '-') {
+    Console.WriteLine("Вы ввели отрицательное число");
+}
+else {
+    Console.WriteLine($"Сумма цифр числа {num} равна {GetSum(num)}");
+}
+
+
+int GetSum(string num){
+int[] array = new int [num.Length];
+for (int i = 0; i < num.Length; i++){
+    array[i] = Convert.ToInt32(num[i].ToString());
+   // Console.WriteLine($"{array[i]}");
+}
+
+int result = 0;
+for (int i = 0; i < array.Length; i++){
+    result += array[i];
+}
+return result;
 }
